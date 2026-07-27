6f6607b4ee472151b7356836df93c7a34c0dd843
Neto Peixoto <joseneto55peixoto@gmail.com>
2026-07-27 19:39:37 -0300

    Registrar os models servico e Agendamento no admin

--- app/agendamentos/admin.py
+++ app/agendamentos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Agendamento
 
-# Register your models here.
+
+@admin.register(Agendamento)
+class AgendamentoAdmin(admin.ModelAdmin):
+    list_display = ('nome_cliente', 'servico', 'data', 'horario', 'status')
+    list_filter = ('status', 'data')
+    search_fields = ('nome_cliente', 'email_cliente')
+    ordering = ('data', 'horario')
--- app/servicos/admin.py
+++ app/servicos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Servico   # Inserindo o models no admin para testar
 
-# Register your models here.
+
+@admin.register(Servico)  # Decorador  
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'duracao_minutos', 'preco', 'ativo')
+    list_filter = ('ativo',)
+    search_fields = ('nome',)
